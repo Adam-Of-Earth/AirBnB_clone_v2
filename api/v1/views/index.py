741d5172c26399e1c20390335e5bdb41f7cc3e9d
Faizan <mathemagician229@gmail.com>
2020-01-29 17:34:53 -0500

    Initiating api directory with init files and tasks.

---
+++ api/v1/views/index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Module which creates a flask route """
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ Returns a JSON status """
+    
+    return jsonify(status="OK")
+
+@app_views.route('/stats', strict_slashes=False)
+def num_objects():
+    """ Returns count of objects by type """
+
+    new_Dict = {}
+    
+    classes = {
+    "Amenity": "amenities",
+    "City": "cities",
+    "Place": "places",
+    "Review": "reviews",
+    "State": "states",
+    "User": "users"
+}
+
+    for key, value in classes.items():
+        new_Dict[value] = models.storage.count(key)
+    return jsonify(new_Dict)
