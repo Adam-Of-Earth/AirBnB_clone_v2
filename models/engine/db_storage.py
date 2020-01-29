9c72e450e6edc3a04eefc08bd82c9a704052853a
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-01-29 00:14:24 -0500

    added functions get and count

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,16 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """A method to retrieve an object from storage"""
+        if classes.get(cls) is None:
+            return None
+        for obj in self.__session.query(classes.get(cls)).filter(
+                classes.get(cls).id == id):
+            return obj
+        return None
+
+    def count(self, cls=None):
+        """a method to count objects in storage"""
+        return (len(self.all(cls)))
