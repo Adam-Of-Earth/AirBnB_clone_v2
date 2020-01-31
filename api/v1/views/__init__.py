4c3d1eb3f7bbca377ac945f3fa9086ca50744e35
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-01-30 22:16:33 -0500

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,12 +5,6 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-<<<<<<< HEAD
 from api.v1.views.states import *
 from api.v1.views.index import *
 from api.v1.views.cities import *
-=======
-from api.v1.views.index import *
-
-
->>>>>>> 4ef47d88179195c5e0758e9b91ea001fbd458b59
