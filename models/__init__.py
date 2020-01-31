237196a64a0672c79e43595e3d471aeb2cc75fdd
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-01-30 23:08:34 -0500

    update

--- models/__init__.py
+++ models/__init__.py
@@ -4,14 +4,20 @@ initialize the models package
 """
 
 from os import getenv
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
 
 
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
+    CNC = db_storage.DBStorage.CNC
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
+    CNC = file_storage.FileStorage.CNC
     storage = FileStorage()
 storage.reload()
