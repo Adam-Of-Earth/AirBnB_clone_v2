312b28a9f8205370da16b52fe3982818a0759510
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-01-30 21:33:02 -0500

    add cities functions

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Flask route that returns json"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+import models
+from models.amenity import Amenity
+from flasgger.utils import swag_from
+
+@app_views.route('/amenities/', strict_slashes=False, methods=['GET', 'POST'])
+@swag_from('swagger_yaml/amenities.yml', methods=['GET', 'POST'])
+def amenities(amenity_id=None):
+    """amenities route with no ID"""
+    if request.method == 'GET':
+        all_amenities = storage.all('Amenity')
+        all_amenities = [obj.to_json() for obj in all_amenities.values()]
+        return jsonify(all_amenities)
+
+    if request.method == 'POST':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        if req_json.get('name') is None:
+            abort(400, 'Missing name')
+        Amenity = CNC.get('Amenity')
+        new_object = Amenity(**req_json)
+        new_object.save()
+        return jsonify(new_object.to_json()), 201
