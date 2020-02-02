152016541fa5f009b8c7f23a26d45b3a0510e4e5
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-02-02 13:18:59 -0500

    removed coma

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """init file to set up flask"""
-from flask import, Blueprint
+from flask import Blueprint
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
