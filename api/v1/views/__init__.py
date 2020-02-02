b15f973645c8e9cf969a34f725b36d66ac02c016
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-02-02 12:00:24 -0500

    Revert "Updating init and executables."
    This reverts commit 2c485efb4d755bd0869a192e23a3c87c35f84164.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,20 +3,8 @@
 from flask import Blueprint
 
 
-
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
 from api.v1.views.states import *
->>>>>>> parent of 4c3d1eb... update
 from api.v1.views.index import *
-from api.v1.views.states import *
 from api.v1.views.cities import *
-=======
-from api.v1.views.index import *
-
-
->>>>>>> 4ef47d88179195c5e0758e9b91ea001fbd458b59
