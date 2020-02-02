bde5d96b0c38b723a241eb79a43dc233c1a527f4
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-02-02 13:12:15 -0500

    removed CNC

--- models/__init__.py
+++ models/__init__.py
@@ -14,10 +14,8 @@ storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
-    CNC = db_storage.DBStorage.CNC
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
-    CNC = file_storage.FileStorage.CNC
     storage = FileStorage()
 storage.reload()
